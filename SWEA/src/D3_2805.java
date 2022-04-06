9e6b4159f758f4ca8a4c941c7f101166df3930eb
Seyoung Shin <pipixpfjs@gmail.com>
2022-04-06 14:45:09 +0900

    2805번 농작물 수확하기

--- SWEA/src/D3_2805.java
+++ SWEA/src/D3_2805.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class D3_2805 { 
+public class D3_2805 {  
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
